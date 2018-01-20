ab056d502d5f7ef9074b1aee962a2a407a10056e
Apoc Arkhesir <adamp218@gmail.com>
2018-01-20 02:27:41 +0100

    Added functionality to digit buttons

--- CSharpCalculator/MainWindow.xaml.cs
+++ CSharpCalculator/MainWindow.xaml.cs
@@ -20,6 +20,25 @@ namespace CSharpCalculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        private float _number1 = 0;
+        private float _number2 = 0;
+        private string _operation = "";
+
+        private void DisplayDigit(int digit)
+        {
+            if (_operation == String.Empty)
+            {
+                _number1 = (_number1 * 10) + digit;
+                Display.Text = _number1.ToString();
+            }
+            else
+            {
+                _number2 = (_number2 * 10) + digit;
+                Display.Text = _number2.ToString();
+            }
+           
+        }
+
         public MainWindow()
         {
             InitializeComponent();
@@ -28,5 +47,54 @@ namespace CSharpCalculator
             this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
         }
 
+        private void Button7_Click(object sender, RoutedEventArgs e)
+        {
+            DisplayDigit(7);
+        }
+
+        private void Button8_Click(object sender, RoutedEventArgs e)
+        {
+            DisplayDigit(8);
+        }
+
+        private void Button9_Click(object sender, RoutedEventArgs e)
+        {
+            DisplayDigit(9);
+        }
+
+        private void Button4_Click(object sender, RoutedEventArgs e)
+        {
+            DisplayDigit(4);
+        }
+
+        private void Button5_Click(object sender, RoutedEventArgs e)
+        {
+            DisplayDigit(5);
+        }
+
+        private void Button6_Click(object sender, RoutedEventArgs e)
+        {
+            DisplayDigit(6);
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            DisplayDigit(1);
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            DisplayDigit(2);
+        }
+
+        private void Button3_Click(object sender, RoutedEventArgs e)
+        {
+            DisplayDigit(3);
+        }
+
+        private void Button0_Click(object sender, RoutedEventArgs e)
+        {
+            DisplayDigit(0);
+        }
     }
 }
